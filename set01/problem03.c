6545c3051ef27d61fb88ed855f3eaf9776bd9eb8
Sumedhavijay04 <115691663+SumedhaVijay04@users.noreply.github.com>
2022-11-10 08:55:48 +0000

    problem done

---
+++ set01/problem03.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter 2 numbers:");
+  scanf("%d%d",&a,&b);
+ int res=add(a,b)
+  printf("%d",res);
+}
+int add(int a,int b)
+{
+  return a+b
+}
