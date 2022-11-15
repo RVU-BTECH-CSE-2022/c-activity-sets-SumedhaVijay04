520cbce385d8043cbca97926838932253f174cfc
Sumedhavijay04 <115691663+SumedhaVijay04@users.noreply.github.com>
2022-11-15 07:00:52 +0000

    problem done

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-int main()
-{
-  printf("Name:Sumedha Vijayaram Kumar");
+int main(){
+ char name[10]="Sumedha";
+  printf("%s", name);
   return 0;
 }
--- set01/problem03.c
+++ set01/problem03.c
@@ -4,10 +4,9 @@ int main()
   int a,b;
   printf("Enter 2 numbers:");
   scanf("%d%d",&a,&b);
- int res=add(a,b)
+ int res=add(a,b);
   printf("%d",res);
 }
 int add(int a,int b)
 {
-  return a+b
-}
+  return a+b;
