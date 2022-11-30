d4f0e2d3f54b60a4d062b7293cd552337ac08961
Sumedha Vijayaram  Vijayaram <>
2022-11-30 09:35:04 +0000

    problem done

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main()
 {
-  int a,b;
+  int a,b,sum;
   printf("Enter 2 numbers:");
   scanf("%d%d",&a,&b);
- int res=add(a,b);
-  printf("%d",res);
+  sum=add(a,b);
+  printf("the sum of %d and %d is %d\n",a,b,sum);
+  return 0;
 }
 int add(int a,int b)
 {
-  return a+b;
+  int sum=a+b;
+  return sum;
+  }
